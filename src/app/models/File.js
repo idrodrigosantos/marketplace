535c9a5a9dfbd07747001cc3704533406b493cab
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-07-24 14:56:24 -0300

    Envio de imagens

---
+++ src/app/models/File.js
@@ -0,0 +1,36 @@
+// Importa a conexão com o banco de dados
+const db = require('../../config/db');
+
+// File System Node.js
+const fs = require('fs');
+
+module.exports = {
+    create({ filename, path, product_id }) {
+        const query = `INSERT INTO files (
+            name,
+            path,
+            product_id
+        ) VALUES ($1, $2, $3)
+        RETURNING id`;
+
+        const values = [
+            filename,
+            path,
+            product_id
+        ];
+
+        return db.query(query, values);
+    },
+    async delete(id) {
+        try {
+            const result = await db.query('SELECT * FROM files WHERE id = $1', [id]);
+            const file = result.rows[0];
+
+            fs.unlinkSync(file.path);
+
+            return db.query('DELETE FROM files WHERE id = $1', [id]);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+};
