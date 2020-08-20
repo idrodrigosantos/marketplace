eb3968a06004ce17ba4abfbc59c76591a410649f
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-08-20 19:45:09 -0300

    Script alterado

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,7 +6,7 @@ const Mask = {
     },
     // Validação do campo valor para aceitar apenas números
     formatBRL(value) {
-        // Aceita apenas números
+        // Limpa o campo
         value = value.replace(/\D/g, '');
 
         // Adiciona R$
@@ -14,6 +14,54 @@ const Mask = {
             style: 'currency',
             currency: 'BRL'
         }).format(value / 100);
+    },
+    cpfCnpj(value) {
+        // Limpa o campo
+        value = value.replace(/\D/g, '');
+
+        if (value.length > 14) {
+            // Se digitar mais de 14 número, tira a última posição
+            value = value.slice(0, -1);
+        }
+
+        // Checa se é CNPJ - 11.222.333/0001-11
+        if (value.length > 11) {
+            // 11222333444455
+            value = value.replace(/(\d{2})(\d)/, '$1.$2');
+            // 11.222333444455
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            // 11.222.333444455
+            value = value.replace(/(\d{3})(\d)/, '$1/$2');
+            // 11.222.333/444455
+            value = value.replace(/(\d{4})(\d)/, '$1-$2');
+            // 11.222.333/4444-55
+        } else {
+            // Checa se é CPF - 111.222.333-44
+            // 11122233344
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            // 111.22233344
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            // 111.222.33344
+            value = value.replace(/(\d{3})(\d)/, '$1-$2');
+            // 111.222.333-44
+        }
+
+        return value;
+    },
+    cep(value) {
+        // Limpa o campo
+        value = value.replace(/\D/g, '');
+
+        if (value.length > 8) {
+            // Se digitar mais de 8 número, tira a última posição
+            value = value.slice(0, -1);
+        }
+
+        // 11111222
+        value = value.replace(/(\d{5})(\d)/, '$1-$2');
+        // 11111-222
+
+        return value;
     }
 };
 
@@ -158,4 +206,75 @@ const Lightbox = {
         Lightbox.target.style.bottom = "initial";
         Lightbox.closeButton.style.top = "-80px";
     },
+};
+
+const Validate = {
+    apply(input, func) {
+        Validate.clearErrors(input);
+
+        let results = Validate[func](input.value);
+        input.value = results.value;
+
+        if (results.error) {
+            Validate.displayError(input, results.error);
+        }
+    },
+    displayError(input, error) {
+        const div = document.createElement('div');
+        div.classList.add('error');
+        div.innerHTML = error;
+        input.parentNode.appendChild(div);
+        input.focus();
+    },
+    clearErrors(input) {
+        const errorDiv = input.parentNode.querySelector('.error');
+
+        if (errorDiv) {
+            errorDiv.remove();
+        }
+    },
+    isEmail(value) {
+        let error = null;
+
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if (!value.match(mailFormat)) {
+            error = 'E-mail inválido.';
+        }
+
+        return {
+            error,
+            value
+        }
+    },
+    isCpfCnpj(value) {
+        let error = null;
+
+        const cleanValues = value.replace(/\D/g, '');
+
+        if (cleanValues.length > 11 && cleanValues.length !== 14) {
+            error = 'CNPJ inválido';
+        } else if (cleanValues.length < 12 && cleanValues.length !== 11) {
+            error = 'CPF inválido';
+        }
+
+        return {
+            error,
+            value
+        }
+    },
+    isCep(value) {
+        let error = null;
+
+        const cleanValues = value.replace(/\D/g, '');
+
+        if (cleanValues.length !== 8) {
+            error = 'CEP inválido';
+        }
+
+        return {
+            error,
+            value
+        }
+    }
 };
