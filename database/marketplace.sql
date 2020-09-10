66deb68d25c19d125ab638d32535e6bfbe4f0671
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-09-10 10:47:03 -0300

    SQL e package.json alterados

--- database/marketplace.sql
+++ database/marketplace.sql
@@ -1,44 +1,52 @@
 CREATE DATABASE marketplace;
 
-CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
-);
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    cpf_cnpj TEXT UNIQUE NOT NULL,
-    cep TEXT,
-    address TEXT,
-    created_at TIMESTAMP DEFAULT (NOW()),
-    updated_at TIMESTAMP DEFAULT (NOW()),
-    reset_token TEXT,
-    reset_token_expires TEXT
-);
-
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    old_price INTEGER,
-    price INTEGER NOT NULL,
-    quantity INTEGER DEFAULT 0,
-    status INTEGER DEFAULT 1,
+    category_id INT NOT NULL,
+    user_id INT,
+    name text NOT NULL,
+    description text NOT NULL,
+    old_price INT,
+    price INT NOT NULL,
+    quantity INT DEFAULT 0,
+    status INT DEFAULT 1,
     created_at TIMESTAMP DEFAULT (NOW()),
-    updated_at TIMESTAMP DEFAULT (NOW()),
-    category_id INTEGER REFERENCES categories (id),
-    user_id INTEGER REFERENCES users (id) ON DELETE CASCADE
+    updated_at TIMESTAMP DEFAULT (NOW())
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL
 );
 
 CREATE TABLE files (
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    path TEXT NOT NULL,
-    product_id INTEGER REFERENCES products (id) ON DELETE CASCADE
+    name text,
+    path text NOT NULL,
+    product_id INT
 );
+
+-- Foreign key
+ALTER TABLE products ADD FOREIGN KEY (category_id) REFERENCES categories (id);
+ALTER TABLE files ADD FOREIGN KEY (product_id) REFERENCES products (id);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL,
+    email text UNIQUE NOT NULL,
+    password text NOT NULL,
+    cpf_cnpj text UNIQUE NOT NULL,
+    cep text,
+    address text,
+    created_at TIMESTAMP DEFAULT (NOW()),
+    updated_at TIMESTAMP DEFAULT (NOW())
+);
+
+-- Foreign key
+ALTER TABLE products ADD FOREIGN KEY (user_id) REFERENCES users (id);
+
+INSERT INTO categories (name) VALUES
+('Comida'), ('Eletrônicos'), ('Automóveis');
 
 -- Create procedure
 CREATE FUNCTION trigger_set_timestamp()
@@ -68,4 +76,19 @@ CREATE TABLE "session" (
     "expire" timestamp(6) NOT NULL
 )
 WITH (OIDS=FALSE);
-ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE session ADD CONSTRAINT session_pkey PRIMARY KEY (sid) NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+-- Token password recovery
+ALTER TABLE users ADD COLUMN reset_token text;
+ALTER TABLE users ADD COLUMN reset_token_expires text;
+
+-- Cascade effect when delete user and products
+ALTER TABLE products DROP CONSTRAINT products_user_id_fkey,
+ADD CONSTRAINT products_user_id_fkey FOREIGN KEY (user_id)
+REFERENCES users (id)
+ON DELETE CASCADE;
+
+ALTER TABLE files DROP CONSTRAINT files_product_id_fkey,
+ADD CONSTRAINT files_product_id_fkey FOREIGN KEY (product_id)
+REFERENCES products (id)
+ON DELETE CASCADE;
